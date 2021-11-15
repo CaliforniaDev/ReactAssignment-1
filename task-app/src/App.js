9cffe48672e949a9f54782a790f31e3aa1ea11f2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-11-14 18:33:41 -0800

    Render form and add a state to our app

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -3,42 +3,23 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      task: { text: ''},
-      tasks: [],
+      taskInput: { text: ''},
+      tasks: []
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.onSubmitTask = this.onSubmitTask.bind(this);
   }
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value
-      }
-    });
-  };
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      task: this.state.tasks.concat(this.state.task),
-      task: { text: ''}
-    });
-  };
   render() {
-    const { task, tasks } = this.state;
     return (
       <div>
         <form>
-          <label htmlFor="taskInput">Enter task</label>
-          <input type="text" id="taskInput" />
-          <button type="submit">
-            Add Task
-          </button>
+          <label htmlFor="taskInput">Enter Tasks</label>
+          <input type="text" id="taskInput"></input>
+          <button type="submit">Submit Task</button>
         </form>
       </div>
-    )
+    );
   }
 }
+
 
 export default App;
