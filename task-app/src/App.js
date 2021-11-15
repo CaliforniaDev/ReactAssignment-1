48990ca5341487f77d152f83dfc2b2e431d153b6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-11-14 18:21:30 -0800

    Add state and event handlers

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: ''},
+      tasks: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitTask = this.onSubmitTask.bind(this);
+  }
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value
+      }
+    });
+  };
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      task: this.state.tasks.concat(this.state.task),
+      task: { text: ''}
+    });
+  };
   render() {
+    const { task, tasks } = this.state;
     return (
-      <h1>Hello World</h1>
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter task</label>
+          <input type="text" id="taskInput" />
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+      </div>
     )
   }
 }
