ed85becaed18243ea181dca95a6b0275fcea7f82
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-11-22 01:16:30 -0800

    Add fontawesome

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 const Overview = (props) => {
     const { tasks } = props;
+    console.log(tasks);
+    const trashIcon = <FontAwesomeIcon icon={faTrashAlt} />
     const listTasks = tasks.map((taskItem) =>
-        <li key={taskItem.id}>{taskItem.text}</li>
+        <li key={taskItem.id}>{taskItem.text} {trashIcon}</li>
     );
+    
     return (
         <div>
+            <FontAwesomeIcon icon={faTrashAlt} />
             <h1>List of Tasks</h1>
             <ul>{listTasks}</ul>
         </div>
