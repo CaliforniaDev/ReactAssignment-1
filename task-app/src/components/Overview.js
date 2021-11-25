f76148a51c8d9d167eea011672a6712490de59aa
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-11-24 23:57:49 -0800

    Refactor

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -5,16 +5,22 @@ import { faEdit, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Overview = (props) => {
     const { tasks, state, removeTask, editTask, updateTask } = props;
-    console.log(tasks);
-    
     const listTasks = tasks.map((taskItem) =>
-    //state.editing === true ? console.log(state) : 
+        //state.editing === true ? console.log(state) : 
         <li className="styles.item" key={taskItem.id}>
             <div>
-            {taskItem.text}
-            <TasksController editTask={editTask} removeTask={() => removeTask(taskItem.id)} />
+                {taskItem.text}
+                <TasksController
+                    editTask={editTask}
+                    removeTask={() => removeTask(taskItem.id)}
+                />
             </div>
-            <input onChange={updateTask} type="text" className="style.textInput" defaultValue={taskItem.text} />
+            <input
+                onChange={updateTask}
+                type="text"
+                className="style.textInput"
+                defaultValue={taskItem.text}
+            />
         </li>
     );
     return (
@@ -29,10 +35,10 @@ const TasksController = (props) => {
     const { removeTask, editTask } = props;
     const deleteIcon = <FontAwesomeIcon className="task-icon" size="lg" icon={faTimesCircle} />;
     const editIcon = <FontAwesomeIcon className="task-icon" size="lg" icon={faEdit} />
-  
+
     return (
         <div id="task-icon-container">
-            <span onClick={removeTask}>{deleteIcon}</span> 
+            <span onClick={removeTask}>{deleteIcon}</span>
             <span onClick={editTask}>{editIcon}</span>
         </div>
     );
