62edd51c31178f9ec652a6a781edc9138c098b4d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-08 16:39:23 -0800

    Change functional component to stateful class handling editing events

--- task-app/src/components/TaskItem.js
+++ task-app/src/components/TaskItem.js
@@ -2,30 +2,45 @@ import React from "react";
 import styles from "./TaskItem.module.scss";
 import TasksController from "./TaskController";
 
-const TaskItem = (props) => {
-    const { handleCompletedTask, deleteTask } = props;
-    const { id, completed, title } = props.task;
-    const completedStyle = {
-        fontStyle: "italic",
-        color: "#595959",
-        opacity: 0.4,
-        textDecoration: "line-through",
-        transition: "0.5s",
+class TaskItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false
+        }
     }
-    return (
-        <li className={styles.item}>
-            <input 
-              type="checkbox"
-              className={styles.checkbox}
-              checked={completed}
-              onChange={() => handleCompletedTask(id)}
-            />
-            <span style={completed ? completedStyle : null}>
-            {title}
-            </span>
-            <TasksController deleteTask={() => deleteTask(id)}/>
-            
-        </li>
-    )
+    handleEditing = () => {
+        this.setState({
+            editing: true
+        })
+    }
+    render() {
+        const { handleCompletedTask, deleteTask } = this.props;
+        const { id, completed, title } = this.props.task;
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+            transition: "0.5s",
+        }
+        return (
+            <li className={styles.item}>
+                <input 
+                  type="checkbox"
+                  className={styles.checkbox}
+                  checked={completed}
+                  onChange={() => handleCompletedTask(id)}
+                />
+                <span style={completed ? completedStyle : null}>
+                {title}
+                </span>
+                <input type="text" className={styles.textInput} />
+                <TasksController deleteTask={() => deleteTask(id)}/>
+                
+            </li>
+        );
+    }
 }
+
 export default TaskItem;
